fd2bac5a4172c916439994015bcaa2e451f654ec
MorryNgo <103199602+MorryNgo@users.noreply.github.com>
2022-06-29 15:14:48 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-    const song = document.getElementById('song');
-    const playbtn = document.querySelector(".player-inner");
+    const song = document.getElementById        ('song');
+    const playbtn = document.querySelector  (".player-inner");
+    const forwardbtn= document.querySelector(".forward")
+    const backbtn = document.querySelector  (".backward")
+    
     let isPlaying = true;
+    let indexSong = 0;
+    
+    forwardbtn.addEventListener('click', function() {
+        console.log ("foward")
+    })
+    
+
+    backbtn.addEventListener('click', function() {
+        console.log ("backward")
+    })
+    
+    
+    
     playbtn.addEventListener ("click",playPause)
     function playPause() {
         if (isPlaying) {
