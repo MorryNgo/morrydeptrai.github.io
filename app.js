ff4462808588e62673fae3996bcda429679f407a
MorryNgo <103199602+MorryNgo@users.noreply.github.com>
2022-06-29 21:48:18 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,19 +2,45 @@
     const playbtn = document.querySelector  (".player-inner");
     const forwardbtn= document.querySelector(".forward")
     const backbtn = document.querySelector  (".backward")
+    const   musics = ['Anh Nhà Ở Đâu Thế  AMEE x B RAY  Masew Remix .mp3','Tinh_Dang_Nhu_Ly_Cafe.mp3'];
     
+
     let isPlaying = true;
     let indexSong = 0;
     
+    
+    
     forwardbtn.addEventListener('click', function() {
         console.log ("foward")
+        changeSong(1);
     })
     
 
     backbtn.addEventListener('click', function() {
         console.log ("backward")
+        changeSong (-1);
     })
     
+    
+    function changeSong(dir) {
+        if (dir===1){
+            indexSong++;
+            isPlaying= true;
+        if (indexSong >= musics.length) {
+            indexSong = 0;
+        } 
+   
+        isPlaying= true; }
+        
+        else if (dir===-1){
+            indexSong--;
+        if (indexSong < 0) 
+        {indexSong = musics.length - 1;} isPlaying = true;
+        }
+
+    song.setAttribute ("src" ,`./music/${musics[indexSong]}`);
+    playPause(`play`)
+}
     
     
     playbtn.addEventListener ("click",playPause)
